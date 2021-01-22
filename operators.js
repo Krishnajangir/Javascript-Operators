231005e4570028cfaad7f415a3b85bc60d0563f5
Krishh <40165326+Krishnajangir@users.noreply.github.com>
2021-01-22 16:07:43 +0530

    modify some syntax issue

--- operators.js
+++ operators.js
@@ -1,12 +1,13 @@
 
 // nullish coalescing operator example
       console.log(null ?? 5) //5
-      connsole.log(5 ?? 3) // 5 
+      console.log(5 ?? 3) // 5 
             
 // Logical nusslish operator
       let x = null;
        let y= undefined;
         let z = 4;
-         console.log(x ?? = z); //4
+         console.log(x ??= z); //4
          console.log(y = (y ?? z)); //4
+         console.log(x , y , z) // 4, 4, 4
          
