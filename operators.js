ef44fb633a72df44cc2e914734e64c1e2ff41549
Krishh <40165326+Krishnajangir@users.noreply.github.com>
2021-01-22 16:03:24 +0530

    Update operators.js

--- operators.js
+++ operators.js
@@ -1,1 +1,12 @@
 
+// nullish coalescing operator example
+      console.log(null ?? 5) //5
+      connsole.log(5 ?? 3) // 5 
+            
+// Logical nusslish operator
+      let x = null;
+       let y= undefined;
+        let z = 4;
+         console.log(x ?? = z); //4
+         console.log(y = (y ?? z)); //4
+         
